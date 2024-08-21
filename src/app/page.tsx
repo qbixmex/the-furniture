1ea932aae67941231074cd9759b5a326a52d32cb
Daniel González Briseño <qbixmex@gmail.com>
2024-08-21 16:50:13 -0700

    Initial Instalation with Default Fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,14 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <h1 className="heading text-8xl">The furniture</h1>
+      <h2 className="heading text-7xl">Subtitle</h2>
+      <h3 className="sub-heading text-6xl">Third Title</h3>
+      <h4 className="sub-heading text-5xl">Fourth Title</h4>
+      <h5 className="sub-heading text-4xl">Fifth Title</h5>
+      <h6 className="sub-heading text-3xl">Sixth Title</h6>
+      <p className="font-thin text-8xl">TH</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, vitae inventore repellat facere sapiente fugit. Inventore, aperiam. Omnis odit, aspernatur iure aut magni debitis ducimus temporibus, ut vel totam accusamus aliquid expedita, quidem voluptatibus consectetur! Sed animi cupiditate minus vero itaque. Labore accusamus dicta cum repellat laboriosam assumenda illum velit inventore error. Quas cum eum consequuntur voluptatibus repellendus illo molestias accusantium cupiditate iste alias, nihil ratione esse nam minus provident laborum architecto est dolore nemo sequi earum odio. Earum eius rem sunt itaque natus id reiciendis aut, architecto accusamus dolor excepturi asperiores quasi esse exercitationem! Obcaecati iusto provident autem. Velit iure asperiores animi quas mollitia ipsum nulla debitis dicta quos officiis eos provident, at quod rem eveniet. Harum ipsa natus sunt ab blanditiis earum ullam maiores, labore minima. Ullam aut ut dolorum beatae similique sint ratione mollitia vitae odio illo cumque tenetur quae perferendis, obcaecati repellendus inventore, quis praesentium in sit quidem delectus corporis aperiam reprehenderit. Ut, quo harum aut placeat, delectus repudiandae assumenda, nobis qui maiores accusamus ullam perspiciatis eligendi? Est ad doloremque adipisci error! Quos omnis deleniti impedit illum suscipit nulla veritatis eaque. In atque dignissimos a corporis perferendis excepturi molestiae alias debitis est eligendi beatae unde asperiores libero accusamus, et laudantium mollitia maiores cupiditate corrupti recusandae nulla quidem placeat id. Odio facere tempore est magni ipsum sunt rem minima quidem repellat quam nisi quos accusamus nulla dignissimos maxime dolores, consequuntur enim quia obcaecati! Exercitationem, corrupti blanditiis! Ipsa odio pariatur quasi ratione ab dolor expedita aspernatur iusto dolorem deleniti, odit quas. Iusto nostrum, ipsum in eveniet explicabo totam officia, voluptate rem ut commodi hic quisquam, vel laborum quia quis ullam odio? Illo esse natus soluta vel omnis perspiciatis, provident id praesentium similique error, tenetur cupiditate atque quisquam corporis magni aut impedit. Aperiam amet ex iste error laboriosam nobis qui repellat rerum asperiores! Dolorum accusamus, in eaque harum repellendus labore id aspernatur aliquam nihil voluptatum, eius sunt vitae doloribus. Maxime fugiat quia quas, molestias sit nostrum? Ullam, tempora suscipit iste illo unde asperiores eum accusamus in. Impedit nulla, quasi a, repellat totam atque earum maiores dolore iusto consectetur obcaecati? Esse omnis hic minus nisi eius repellendus, voluptatibus maxime soluta, quaerat, architecto repellat similique obcaecati dolorum maiores suscipit aperiam animi praesentium exercitationem aut ipsam consectetur amet quasi porro. Ratione quia quis quam quae voluptatem unde rem laboriosam modi nostrum error debitis cupiditate at, eos, ipsam aliquid officiis asperiores, atque omnis veniam. Commodi ad possimus minus itaque eligendi explicabo repudiandae eveniet facere, nemo laborum illum quo unde quaerat et mollitia earum numquam iure sequi voluptas pariatur placeat aut natus voluptatibus officiis. Ex explicabo consectetur, omnis, optio ipsam quidem voluptatum obcaecati commodi ea quod ipsa unde totam. Quod excepturi itaque repellendus, possimus explicabo veritatis, ab repudiandae asperiores illo doloribus molestias. Aspernatur iure unde ut dignissimos similique architecto dolore reprehenderit at eveniet aperiam veritatis tempora necessitatibus quae pariatur accusamus ex perspiciatis earum dicta repudiandae, doloribus officiis ipsa nesciunt provident repellat. Cum animi ea hic et laudantium dolores eveniet aliquid, velit cumque facilis temporibus.</p>
+    </div>
   );
 }
---
+++ src/fonts/fonts.ts
@@ -0,0 +1,120 @@
+import { Livvic } from 'next/font/google'
+import localFont from "@next/font/local";
+ 
+export const livvic = Livvic({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "900"],
+  variable: "--font-livvic",
+});
+
+export const helveticaNeue = localFont({
+  src: [
+    {
+      path: "./helvetica-neue-5/HelveticaNeueUltraLight.otf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueUltraLightItalic.otf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueLight.otf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueLightItalic.otf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueThin.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueThinItalic.otf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueMedium.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueMediumItalic.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueRoman.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueRoman.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueBold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueBoldItalic.otf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueBlack.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueBlackItalic.otf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueHeavy.otf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./helvetica-neue-5/HelveticaNeueHeavyItalic.otf",
+      weight: "800",
+      style: "italic",
+    },
+  ],
+  variable: "--font-helvetica-neue",
+});
+
+export const copperplate = localFont({
+  src: [
+    {
+      path: "./copperplate/OPTICopperplate-Light.otf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./copperplate/OPTICopperplate.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./copperplate/OPTICopperplate-Heavy.otf",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+  variable: "--font-copperplate",
+});
